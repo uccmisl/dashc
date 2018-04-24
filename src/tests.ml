b27da8c028f9f75e28ec818d1f53d5af2319a16d
Aleksandr Reviakin <ar11@cs.ucc.ie>
2018-04-24 21:45:29 +0100

    A few tests related to mpd parsing were added

--- src/tests.ml
+++ src/tests.ml
@@ -18,15 +18,50 @@
  *	02110-1301, USA.
 *)
 
+open Core
 open Playback
+open Representation
+
+let mpd_file_name_1 = "../../../test_mpd/bbb_enc_10min_x264_dash.mpd"
+let mpd_file_segment_duration_1 = 4
+
+let parse_mpd file_name =
+  let body = In_channel.read_all mpd_file_name_1 in
+  let mpd = Xml.parse_string body in
+  let representations : (int, representation) Hashtbl.t = repr_table_from_mpd mpd in
+  representations
 
 let input_algorithm () =
   Alcotest.check Alcotest.unit
     "BBA-2 is supported" () (check_input_algorithm_existence "bba-2")
 
+let mpd_parsing_bandwidth () =
+  let representations = parse_mpd mpd_file_name_1 in
+  Alcotest.check Alcotest.int "The bandwidth value in the given mpd file is"
+  4275756
+  (Hashtbl.find_exn representations 10).bandwidth
+
+let mpd_parsing_repr_number () =
+  let representations = parse_mpd mpd_file_name_1 in
+  Alcotest.check Alcotest.int "The number of representations in the given mpd file is"
+  10
+  (Hashtbl.length representations)
+
+let test_get_last_segment_index () =
+  let body = In_channel.read_all mpd_file_name_1 in
+  let mpd = Xml.parse_string body in
+  let last_segment_index =
+    get_last_segment_index mpd mpd_file_segment_duration_1 None in
+  Alcotest.check Alcotest.int "The last segment index is"
+  150
+  last_segment_index
+
 let () =
   Alcotest.run "tests" [
     "first", [
-      "input algorithm", `Quick, input_algorithm;
+      "input algorithm availability", `Quick, input_algorithm;
+      "mpd bandwidth parsing", `Quick, mpd_parsing_bandwidth;
+      "mpd number of representations parsing", `Quick, mpd_parsing_repr_number;
+      "get_last_segment_index fun", `Quick, test_get_last_segment_index;
     ];
   ]
