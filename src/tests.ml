c11ffd3f42b8eea2c69e9c165b30eeeb4b10c94b
Aleksandr Reviakin <ar11@cs.ucc.ie>
2018-04-30 11:22:13 +0100

    Tests for one more mpd were added

--- src/tests.ml
+++ src/tests.ml
@@ -27,35 +27,59 @@ let mpd_file_name_1 = "../../../test_mpd/bbb_enc_10min_x264_dash.mpd"
 let mpd_file_segment_duration_1 = 4
 let mpdf_file_link_1 = "https://127.0.0.1/bbb_enc_10min_x264_dash.mpd"
 
-let parse_mpd file_name =
-  let body = In_channel.read_all mpd_file_name_1 in
-  let mpd = Xml.parse_string body in
-  let representations : (int, representation) Hashtbl.t = repr_table_from_mpd mpd in
-  representations
+let mpd_file_name_2 = "../../../test_mpd/tearsofsteel_enc_x264_dash.mpd"
+let mpd_file_segment_duration_2 = 6
 
 let input_algorithm () =
   Alcotest.check Alcotest.unit
     "BBA-2 is supported" () (check_input_algorithm_existence "bba-2")
 
-let mpd_parsing_bandwidth () =
+let parse_mpd file_name =
+  let body = In_channel.read_all file_name in
+  let mpd = Xml.parse_string body in
+  let representations : (int, representation) Hashtbl.t = repr_table_from_mpd mpd in
+  representations
+
+let mpd_parsing_bandwidth_1 () =
   let representations = parse_mpd mpd_file_name_1 in
   Alcotest.check Alcotest.int "The bandwidth value in the given mpd file is"
   4275756
   (Hashtbl.find_exn representations 10).bandwidth
 
-let mpd_parsing_repr_number () =
+let mpd_parsing_bandwidth_2 () =
+  let representations = parse_mpd mpd_file_name_2 in
+  Alcotest.check Alcotest.int "The bandwidth value in the given mpd file is"
+  236378
+  (Hashtbl.find_exn representations 1).bandwidth
+
+let mpd_parsing_repr_number_1 () =
   let representations = parse_mpd mpd_file_name_1 in
   Alcotest.check Alcotest.int "The number of representations in the given mpd file is"
   10
   (Hashtbl.length representations)
 
-let test_get_last_segment_index () =
+let mpd_parsing_repr_number_2 () =
+  let representations = parse_mpd mpd_file_name_2 in
+  Alcotest.check Alcotest.int "The number of representations in the given mpd file is"
+  13
+  (Hashtbl.length representations)
+
+let test_get_last_segment_index_1 () =
   let body = In_channel.read_all mpd_file_name_1 in
   let mpd = Xml.parse_string body in
   let last_segment_index =
     get_last_segment_index mpd mpd_file_segment_duration_1 None in
   Alcotest.check Alcotest.int "The last segment index is"
   150
+  last_segment_index
+
+let test_get_last_segment_index_2 () =
+  let body = In_channel.read_all mpd_file_name_2 in
+  let mpd = Xml.parse_string body in
+  let last_segment_index =
+    get_last_segment_index mpd mpd_file_segment_duration_2 None in
+  Alcotest.check Alcotest.int "The last segment index is"
+  123
   last_segment_index
 
 let test_read_segment_size_file () =
@@ -83,9 +107,12 @@ let () =
   Alcotest.run "tests" [
     "first", [
       "input algorithm availability", `Quick, input_algorithm;
-      "mpd bandwidth parsing", `Quick, mpd_parsing_bandwidth;
-      "mpd number of representations parsing", `Quick, mpd_parsing_repr_number;
-      "get_last_segment_index fun", `Quick, test_get_last_segment_index;
+      "mpd, bandwidth file 1", `Quick, mpd_parsing_bandwidth_1;
+      "mpd, bandwidth file 2", `Quick, mpd_parsing_bandwidth_2;
+      "mpd, number of representations file 1", `Quick, mpd_parsing_repr_number_1;
+      "mpd, number of representations file 2", `Quick, mpd_parsing_repr_number_2;
+      "mpd, get_last_segment_index, file 1", `Quick, test_get_last_segment_index_1;
+      "get_last_segment_index, file 2", `Quick, test_get_last_segment_index_2;
       "read_segment_size_file fun", `Slow, test_read_segment_size_file;
     ];
   ]
